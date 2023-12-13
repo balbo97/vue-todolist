ef4e35ab330966d85d4309c309eeeeba71d089fe
balbo97 <balbosauro1@gmail.com>
2023-12-13 21:44:23 +0100

    MILESTONE 3 completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            new_task: '',
             todo_list: [
                 {
                     text: 'allenarsi',
@@ -33,6 +34,14 @@ createApp({
     methods: {
         remove_task(index){
             this.todo_list.splice(index,1)
+        },
+        add_newTask(){
+            this.todo_list.push({
+                text: this.new_task,
+                done: false
+            });
+
+            this.new_task=''
         }
     }
 }).mount("#app")
